4a81e845b7c4f45795505d7edd13fb4268e836dc
nataliamoran <natalja.moran@gmail.com>
2020-05-30 10:55:36 -0400

    added guidelines

--- a1-mobile/src/config.js
+++ a1-mobile/src/config.js
@@ -1,8 +1,4 @@
-// PRODUCTION
-// export const SERVER_URL = window.location.origin + '/';
-// const BASE_URL = window.location.origin + '/api';
-
-// LOCAL
-const BASE_URL = 'https://csc301-a1-nmab.herokuapp.com/api';
+// const BASE_URL = 'https://csc301-a1-nmab.herokuapp.com/api';
+const BASE_URL = 'http://localhost:5000/api';
 export const CART = `${BASE_URL}/cart`;
 export const ITEMS = `${BASE_URL}/items`;
